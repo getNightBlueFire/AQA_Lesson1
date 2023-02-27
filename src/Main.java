461b59591e0a88a9cb3e16c9d3cbe294e7d16ed1
Dmitry Hotko <d.hotko@invento.by>
2023-02-27 11:34:14 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
 	public static void main (String[] args) {
 		System.out.println("Hello world!");
+		main1();
+		main2();
 	}
+
+	public static void main1 () {
+		System.out.println("Hello world!_1");
+	}
+
+	public static void main2 () {
+		System.out.println("Hello world!_2");
+	}
+
 }
